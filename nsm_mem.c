d43cacea1d5927d7b507865a805a83c8b84b1c0e
AMIRMNOOHI <highlimner@gmail.com>
2023-04-24 03:54:41 +0000

    + NSM Memory

---
+++ nsm_mem.c
@@ -0,0 +1,31 @@
+#include "nsm_mem.h"
+
+__memory_t *memory;
+
+void __memory_init(void){
+    size_t i;
+
+    memory = kmalloc(sizeof(__memory_t), GFP_KERNEL);
+    memory->pages = kmalloc(sizeof(__page_t) * DIV_ROUND_UP(BUF_LEN, PAGE_SIZE), GFP_KERNEL);
+    memory->size = DIV_ROUND_UP(BUF_LEN, PAGE_SIZE);
+
+    
+    // for each page allocate page
+    for (i = 0; i < memory->size; i++)
+    {
+        memory->pages[i].__page = alloc_page(GFP_KERNEL);
+        memory->pages[i].state = __INVALID;
+    }
+}
+
+void __memory_destroy(void){
+    size_t i;
+    
+    for (i = 0; i < memory->size; i++)
+    {
+        __free_page(memory->pages[i].__page);
+    }
+    kfree(memory->pages);
+    kfree(memory);
+}
+
